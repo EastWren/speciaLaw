7e78f14a563ecdaef515daecc8c2b5a6f7007671
CamWren <wrnecmrn@gmail.com>
2018-03-13 21:16:33 -0500

    Forming layouts - header and footer specifically

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,16 +9,29 @@ const ListLink = props =>
   </li>
 
 export default ({ children }) =>
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
-    <header style={{ marginBottom: `1.5rem` }}>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>MySweetSite</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children()}
+  <div>
+    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>MySweetSite</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/AttorneyProfile/">Attorney Profile</ListLink>
+          <ListLink to="/FamilyLaw/">Family Law</ListLink>
+          <ListLink to="/Contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children()}
+    </div>
+    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+      <footer style={{ marginBottom: `1.5rem` }}>
+        <ul style={{ listStyle: `none` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/AttorneyProfile/">Attorney Profile</ListLink>
+          <ListLink to="/FamilyLaw/">Family Law</ListLink>
+          <ListLink to="/Contact/">Contact</ListLink>
+        </ul>
+      </footer>
+    </div>
   </div>
